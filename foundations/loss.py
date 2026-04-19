361ba2bd56cd89a232a40246149f81533e9e2d79
Navnath Kadam <navnathkadam284@gmail.com>
2026-04-19 20:53:10 +0530

    Update Cross-entropy loss (Math Foundations 4/6)

--- foundations/loss.py
+++ foundations/loss.py
@@ -9,6 +9,10 @@ class Solution:
         # y_pred: predicted probabilities
         # Hint: add a small epsilon (1e-7) to y_pred to avoid log(0)
         # return round(your_answer, 4)
+        
+        # protects from log(0) inf
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+        
         res = -np.mean((y_true) * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
         return np.round(res, 4)
 
@@ -20,5 +24,9 @@ class Solution:
         
         # y_true = [[1,0,0] ,       [0,1,0]] 
         # y_pred = [[0.4,0.2,0.1] , [0.1, 0.5,0.9]]
+        
+        # protects from log(0) inf
+        y_pred = np.clip(y_pred, 1e-7, 1 - 1e-7)
+
         s = np.sum(y_true * y_pred, axis=1)
         return np.round(-np.mean((np.log(s))), 4)
